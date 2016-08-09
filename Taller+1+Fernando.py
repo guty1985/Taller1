44dc7b40277ee9a553cd670b8e09a05cf4b36203
guty1985 <guty-15@hotmail.com>
2016-08-08 20:28:56 -0500

    Creacion Taller 1

---
+++ Taller+1+Fernando.py
@@ -0,0 +1,102 @@
+
+# coding: utf-8
+
+# In[5]:
+
+print "Formula Matematica"
+
+
+# $$\sum_{p}{}li(x^p)-log(2)+\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$$
+
+# In[6]:
+
+2+4*6/8.0-3
+
+
+# In[8]:
+
+3.0/2
+
+
+# In[9]:
+
+3+5j
+
+
+# In[11]:
+
+(3+5j) - (1-1j)
+
+
+# In[14]:
+
+abs(3+5j)
+
+
+# In[15]:
+
+5**4
+
+int(_14)
+# In[19]:
+
+round(_14)
+
+
+# In[20]:
+
+complex(_15)
+
+
+# In[24]:
+
+max(1,3,-5)
+
+
+# In[25]:
+
+min(1,5,-5)
+
+
+# In[26]:
+
+a=5
+
+
+# In[27]:
+
+print a
+
+
+# In[28]:
+
+x,y=5,3
+
+
+# In[29]:
+
+print x
+
+
+# In[30]:
+
+print y
+
+
+# In[37]:
+
+z=(x+y)
+
+
+# In[38]:
+
+print(x); print (y); print(z)
+
+
+# In[49]:
+
+if x>y:
+    print "El numero mayor es",x
+else: 
+    print "El numero mayor es",y
+
